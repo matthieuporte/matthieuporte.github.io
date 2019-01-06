0091751bdbbb8398381b744ad354a2add82bdcbe
matthieuporte <nakror4@gmail.com>
2019-01-06 12:53:28 +0100

    add news projects to my website

---
+++ js/calculette.js
@@ -0,0 +1,25 @@
+// var $btn7 = $(".btn7");
+// var $btn8 = $(".btn8");
+// var $btn9 = $(".btn9");
+// var $btn4 = $(".btn4");
+// var $btn5 = $(".btn5");
+// var $btn6 = $(".btn6");
+// var $btn1 = $(".btn1");
+// var $btn2 = $(".btn2");
+// var $btn3 = $(".btn3");
+// var $btn0 = $(".btn0");
+// var $btnpoint = $(".btnpoint");
+// var $btnresultgive = $(".btnresultgive");
+// var $btndivise = $(".btndivise");
+// var $btnmultiple = $(".btnmultiple");
+// var $btnsoustract = $(".btnsoustract");
+// var $btnadd = $(".btnadd");
+
+// var calcul = "";
+
+
+// for (var i = 9; i >= 0; i--) {
+// 	document.getElementsByClassName("btn"+i)[0].addEventListener("click", function(){
+// 		calcul.concat(i);
+// 	});
+// }
